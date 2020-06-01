32eff7637b83476d1b11a1f5cbdd1c9736a2a448
Azhar Ali <azhar.ali@imuons.com>
2020-06-01 15:19:40 +0530

    Commit by azhar

--- app/src/main/java/com/imuons/saddaadda/ReportAdapter.java
+++ app/src/main/java/com/imuons/saddaadda/ReportAdapter.java
@@ -1,34 +1,167 @@
 package com.imuons.saddaadda;
 
+import android.app.Activity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.imuons.saddaadda.DataModel.ReportData;
+import com.imuons.saddaadda.View.ReportActivity;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
 public class ReportAdapter extends RecyclerView.Adapter<ReportAdapter.ReportHolder> {
+    Activity activity;
+    ArrayList<ReportData> reportDataArrayList;
+
+    public ReportAdapter(Activity activity, ArrayList<ReportData> reportDataArrayList) {
+        this.activity = activity;
+        this.reportDataArrayList = reportDataArrayList;
+    }
+
     @NonNull
     @Override
     public ReportHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View itemView = LayoutInflater.from(parent.getContext())
-                .inflate(R.layout.item_report, parent, false);
+                .inflate(R.layout.item_report_new, parent, false);
         return new ReportHolder(itemView);
     }
 
     @Override
     public void onBindViewHolder(@NonNull ReportHolder holder, int position) {
+        if (position != 0) {
+            holder.top_bar.setVisibility(View.GONE);
+            holder.nxt.setVisibility(View.VISIBLE);
+            ReportData reportData = reportDataArrayList.get(position);
+            if (reportData.isOpen()) {
+                holder.dropdown_menu.setVisibility(View.VISIBLE);
+                holder.expend_image.setImageDrawable(activity.getResources().getDrawable(R.drawable.expend_1));
+            } else {
+                holder.dropdown_menu.setVisibility(View.GONE);
+                holder.expend_image.setImageDrawable(activity.getResources().getDrawable(R.drawable.expend));
+            }
+            holder.sno.setText(String.valueOf(position));
+            holder.amt.setText(String.valueOf(reportData.getAmount()));
+            holder.cBal.setText(String.valueOf(reportData.getBalance()));
+            holder.pBal.setText(String.valueOf(reportData.getPrevBalance()));
+            holder.remark.setText(reportData.getRemark());
+           /* if (reportData.getTrType().equalsIgnoreCase("Debit")) {
+                holder.tranType.setText("DR");
+            } else {
+                holder.tranType.setText("CR");
+            }*/
+            holder.tranDate.setText(parseDate(reportData.getEntryTime()));
+            //holder.type.setText(reportData.getType());
+           /* if(position == reportDataArrayList.size()-1){
+                holder.botto_line.setVisibility(View.VISIBLE);
+            }else
+                holder.botto_line.setVisibility(View.GONE);*/
+        } else {
+            holder.top_bar.setVisibility(View.VISIBLE);
+            holder.nxt.setVisibility(View.GONE);
+        }
+    }
+
+    private String parseDate(String entryTime) {
+        String inputPattern = "yyyy-MM-dd HH:mm:ss";
+        String outputPattern = "dd-MMM-yy";
+        SimpleDateFormat inputFormat = new SimpleDateFormat(inputPattern);
+        SimpleDateFormat outputFormat = new SimpleDateFormat(outputPattern);
+
+        Date date = null;
+        String str = null;
+
+        try {
+            date = inputFormat.parse(entryTime);
+            str = outputFormat.format(date);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        return str;
 
     }
 
     @Override
     public int getItemCount() {
-        return 5;
+        return reportDataArrayList.size();
+    }
+
+    public void update(ArrayList<ReportData> reportData) {
+        reportDataArrayList = reportData;
+        notifyDataSetChanged();
+    }
+
+    public void update(ArrayList<ReportData> reportData, int pos) {
+        reportDataArrayList = reportData;
+        notifyItemChanged(pos);
     }
 
     public class ReportHolder extends RecyclerView.ViewHolder {
+        @BindView(R.id.sno)
+        TextView sno;
+
+        @BindView(R.id.pBal)
+        TextView pBal;
+
+        /*@BindView(R.id.type)
+        TextView type;
+*/
+        @BindView(R.id.amt)
+        TextView amt;
+
+        @BindView(R.id.tranType)
+        TextView tranType;
+        @BindView(R.id.cBal)
+        TextView cBal;
+        @BindView(R.id.tranDate)
+        TextView tranDate;
+        @BindView(R.id.remark)
+        TextView remark;
+
+        @BindView(R.id.nxt)
+        RelativeLayout nxt;
+
+        @BindView(R.id.expend_list)
+        RelativeLayout expend_list;
+
+        @BindView(R.id.expend_image)
+        ImageView expend_image;
+
+        /*@BindView(R.id.top)
+        RelativeLayout top;*/
+        @BindView(R.id.top_bar)
+        LinearLayout top_bar;
+        @BindView(R.id.dropdown_menu)
+        RelativeLayout dropdown_menu;
+
+
+       /* @BindView(R.id.botto_line)
+        View botto_line;*/
+
+
         public ReportHolder(@NonNull View itemView) {
             super(itemView);
+            ButterKnife.bind(this, itemView);
+        }
+
+        @OnClick({R.id.expend_list ,R.id.expend_image , R.id.sno })
+        void expend() {
+            ((ReportActivity) activity).checkOpen(getAdapterPosition());
+
         }
     }
 }
--- app/src/main/java/com/imuons/saddaadda/View/ReportActivity.java
+++ app/src/main/java/com/imuons/saddaadda/View/ReportActivity.java
@@ -9,17 +9,22 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.app.Dialog;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.gson.Gson;
+import com.imuons.saddaadda.DataModel.ReportData;
 import com.imuons.saddaadda.EntityClass.OtpEnitity;
 import com.imuons.saddaadda.R;
 import com.imuons.saddaadda.ReportAdapter;
 import com.imuons.saddaadda.Utils.AppCommon;
 import com.imuons.saddaadda.Utils.ViewUtils;
+import com.imuons.saddaadda.responseModel.ReportResponse;
 import com.imuons.saddaadda.responseModel.VerifyUserResponse;
 import com.imuons.saddaadda.retrofit.AppService;
 import com.imuons.saddaadda.retrofit.ServiceGenerator;
+
+import java.util.ArrayList;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -33,13 +38,21 @@ public class ReportActivity extends AppCompatActivity {
     RecyclerView recycleView;
     ReportAdapter reportAdapter;
 
+    ArrayList<ReportData> reportData;
+    @BindView(R.id.txUserId)
+    TextView txUserId;
+    @BindView(R.id.coin)
+    TextView coin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_report);
         ButterKnife.bind(this);
-        reportAdapter = new ReportAdapter();
+        coin.setText(String.valueOf(AppCommon.getInstance(this).getAccount()));
+        txUserId.setText(String.valueOf(AppCommon.getInstance(this).getUserId()));
+        reportData = new ArrayList<>();
+        reportAdapter = new ReportAdapter(this , reportData);
         RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(this);
         recycleView.setLayoutManager(mLayoutManager);
 
@@ -53,17 +66,21 @@ public class ReportActivity extends AppCompatActivity {
         if (AppCommon.getInstance(this).isConnectingToInternet(this)) {
             Dialog dialog = ViewUtils.getProgressBar(ReportActivity.this);
             AppService apiService = ServiceGenerator.createService(AppService.class);
-            Call call = apiService.checkUserApi(new OtpEnitity(""));
+            Call call = apiService.REPORT_CALL();
             call.enqueue(new Callback() {
                 @Override
                 public void onResponse(Call call, Response response) {
                     AppCommon.getInstance(ReportActivity.this).clearNonTouchableFlags(ReportActivity.this);
                    dialog.dismiss();
-                    VerifyUserResponse authResponse = (VerifyUserResponse) response.body();
+                    ReportResponse authResponse = (ReportResponse) response.body();
                     if (authResponse != null) {
                         Log.i("Response::", new Gson().toJson(authResponse));
                         if (authResponse.getCode() == 200) {
-
+                            reportData = authResponse.getData();
+                            if(reportData.size() !=0) {
+                                reportData.add(0 ,new ReportData());
+                                reportAdapter.update(reportData);
+                            }
                         } else {
                              Toast.makeText(ReportActivity.this, authResponse.getMessage(), Toast.LENGTH_SHORT).show();
                         }
@@ -81,10 +98,18 @@ public class ReportActivity extends AppCompatActivity {
                 }
             });
 
-
         } else {
             // no internet
             Toast.makeText(this, "Please check your internet", Toast.LENGTH_SHORT).show();
         }
+    }
+
+    public void checkOpen(int adapterPosition) {
+        if(reportData.get(adapterPosition).isOpen()){
+            reportData.get(adapterPosition).setOpen(false);
+        }else {
+            reportData.get(adapterPosition).setOpen(true);
+        }
+        reportAdapter.update(reportData , adapterPosition);
     }
 }
