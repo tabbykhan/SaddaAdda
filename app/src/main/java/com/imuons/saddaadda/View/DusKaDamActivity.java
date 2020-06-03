ff7e022f000416693d90c759778dab95c4d9bad9
Tabish khan <tabishskhn@gmail.com>
2020-06-03 13:06:50 +0530

    Commit By Tabish

--- app/src/main/java/com/imuons/saddaadda/View/DusKaDamActivity.java
+++ app/src/main/java/com/imuons/saddaadda/View/DusKaDamActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageView;
 import android.widget.RelativeLayout;
 
 import com.imuons.saddaadda.R;
@@ -16,6 +17,97 @@ public class DusKaDamActivity extends AppCompatActivity {
 
     @BindView(R.id.gotoRules)
     RelativeLayout gotoRules;
+
+    @BindView(R.id.one)
+    ImageView one;
+    @BindView(R.id.two)
+    ImageView two;
+    @BindView(R.id.three)
+    ImageView three;
+    @BindView(R.id.four)
+    ImageView four;
+    @BindView(R.id.five)
+    ImageView five;
+    @BindView(R.id.six)
+    ImageView six;
+    @BindView(R.id.seven)
+    ImageView seven;
+    @BindView(R.id.eight)
+    ImageView eight;
+    @BindView(R.id.nine)
+    ImageView nine;
+    @BindView(R.id.ten)
+    ImageView ten;
+
+    @BindView(R.id.eleven)
+    ImageView eleven;
+    @BindView(R.id.twelve)
+    ImageView twelve;
+    @BindView(R.id.thirteen)
+    ImageView thirteen;
+    @BindView(R.id.fourteen)
+    ImageView fourteen;
+    @BindView(R.id.fifteen)
+    ImageView fifteen;
+    @BindView(R.id.sixteen)
+    ImageView sixteen;
+    @BindView(R.id.seventeen)
+    ImageView seventeen;
+    @BindView(R.id.eighteen)
+    ImageView eighteen;
+    @BindView(R.id.nineteen)
+    ImageView nineteen;
+    @BindView(R.id.twenty)
+    ImageView twenty;
+
+    @BindView(R.id.twentyOne)
+    ImageView twentyOne;
+    @BindView(R.id.twentyTwo)
+    ImageView twentyTwo;
+    @BindView(R.id.twentyThree)
+    ImageView twentyThree;
+    @BindView(R.id.twentyFour)
+    ImageView twentyFour;
+    @BindView(R.id.twentyFive)
+    ImageView twentyFive;
+    @BindView(R.id.twentySix)
+    ImageView twentySix;
+    @BindView(R.id.twentySeven)
+    ImageView twentySeven;
+    @BindView(R.id.twentyEight)
+    ImageView twentyEight;
+    @BindView(R.id.twentyNine)
+    ImageView twentyNine;
+    @BindView(R.id.thirty)
+    ImageView thirty;
+
+    @BindView(R.id.thirtyOne)
+    ImageView thirtyOne;
+    @BindView(R.id.thirtyTwo)
+    ImageView thirtyTwo;
+    @BindView(R.id.thirtyThree)
+    ImageView thirtyThree;
+    @BindView(R.id.thirtyFour)
+    ImageView thirtyFour;
+    @BindView(R.id.thirtyFive)
+    ImageView thirtyFive;
+    @BindView(R.id.thirtySix)
+    ImageView thirtySix;
+    @BindView(R.id.thirtySeven)
+    ImageView thirtySeven;
+
+    String Product_idOne = "1";String Product_idTwo ="2";String Product_idThree ="3";String Product_idFour ="4";String Product_idFive ="5";
+    String Product_idSix = "6";String Product_idSeven ="7";String Product_idEight ="8";String Product_idNine ="9";String Product_idTen ="10";
+
+    String Product_idEleven = "11";String Product_idTwelve ="12";String Product_idThirteen ="13";String Product_idFourteen ="14";String Product_idFifteen ="15";
+    String Product_idSixteen = "16";String Product_idSeventeen ="17";String Product_idEighteen ="18";String Product_idNineteen ="19";String Product_idTwenty ="20";
+
+    String Product_idTwentyOne = "21";String Product_idTwentyTwo ="22";String Product_idTwentyThree ="23";String Product_idTwentyFour ="24";String Product_idTwentyFive ="25";
+    String Product_idTwentySix = "26";String Product_idTwentySeven ="27";String Product_idTwentyEight ="28";String Product_idTwentyNine ="29";String Product_idThirty ="30";
+
+    String Product_idThirtyOne ="31"; String Product_idThirtyTwo ="32"; String Product_idThirtyThree ="33";
+    String Product_idThirtyFour ="34"; String Product_idThirtyFive ="35"; String Product_idThirtySix ="36";
+    String Product_idThirtySeven ="37";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
