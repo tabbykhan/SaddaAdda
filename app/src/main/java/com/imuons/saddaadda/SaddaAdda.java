bf103033b98826acd01616cd78f261d25f285a1d
Rahul Vhayaskar <r.vhayaskar@adlersolutions.in>
2020-06-15 17:51:47 +0530

    Merge branch 'master' of https://github.com/tabbykhan/SaddaAdda
     Conflicts:
    	app/src/main/AndroidManifest.xml
    	app/src/main/java/com/imuons/saddaadda/retrofit/AppService.java
    	app/src/main/res/values/strings.xml

--- app/src/main/java/com/imuons/saddaadda/SaddaAdda.java
+++ app/src/main/java/com/imuons/saddaadda/SaddaAdda.java
@@ -10,6 +10,7 @@ import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
 
 public class SaddaAdda extends Application {
     public static ImageLoader imageLoader;
+
     @Override
     public void onCreate() {
         super.onCreate();
@@ -17,6 +18,7 @@ public class SaddaAdda extends Application {
         FirebaseApp.initializeApp(this);
         if(imageLoader==null){
             intImageLoader();
+           // getTranslateService();
         }
     }
     private void intImageLoader() {
@@ -33,4 +35,37 @@ public class SaddaAdda extends Application {
             imageLoader=imageLoader.getInstance();
         }
     }
+//    public void getTranslateService() {
+//
+//        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+//        StrictMode.setThreadPolicy(policy);
+//
+//        try (InputStream is =new FileInputStream("AIzaSyBVsRPq3hKB8R89fCcSM9JLR2Jq1xuAr9A")) {
+//
+//            //Get credentials:
+//            final GoogleCredentials myCredentials = GoogleCredentials.fromStream(is);
+//
+//            //Set credentials and get translate service:
+//            TranslateOptions translateOptions = TranslateOptions.newBuilder().setCredentials(myCredentials).build();
+//            translate = translateOptions.getService();
+//
+//        } catch (IOException ioe) {
+//            ioe.printStackTrace();
+//
+//        }
+//    }
+//
+//    public String translate(String input,String type) {
+//
+//        //Get input text to be translated:
+//        String originalText = input;
+//        Translation translation = translate.translate(originalText,
+//                Translate.TranslateOption.targetLanguage(type), Translate.TranslateOption.model(
+//                        "base"));
+//        String translatedText = translation.getTranslatedText();
+//
+//        //Translated text and original text are set to TextViews:
+//       return translatedText;
+//
+//    }
 }
